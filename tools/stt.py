4b9c8e5eb9e134eaf0425ed1fcfebafcd10b2952
subrat <subratsingh010@gmail.com>
2025-10-03 12:53:56 +0530

    changes model

--- tools/stt.py
+++ tools/stt.py
@@ -11,7 +11,15 @@ import os
 # Device and Model
 # -------------------------------
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("medium").to('cpu')  # Use 'cpu' or 'cuda'
+model = whisper.load_model("turbo").to('cpu')  # Use 'cpu' or 'cuda'
+#Models (tiny, base, small, medium, large,turbo)
+# Size	Parameters	English-only model	Multilingual model	Required VRAM	Relative speed
+# tiny	39 M	tiny.en	tiny	~1 GB	~10x
+# base	74 M	base.en	base	~1 GB	~7x
+# small	244 M	small.en	small	~2 GB	~4x
+# medium 769 M	medium.en	medium	~5 GB	~2x
+# large	1550 M	N/A	large	~10 GB	1x
+# turbo	809 M	N/A	turbo	~6 GB	~8x
 
 # -------------------------------
 # Utilities
